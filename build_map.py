8659946aca92ce27235e7726f3c0cdd3ca3cbaec
Martin Malina <mmalina@redhat.com>
2021-05-17 08:04:45 +0200

    Load activities in batches and add more details to map
    We now fetch 10 activities at a time and use a generator.
    
    Also, there are now more details in the markers on the map
    including links back to the activity on Strava.

--- build_map.py
+++ build_map.py
@@ -5,8 +5,21 @@ import folium
 
 from get_access_token import get_access_token
 
-SINCE = "2020-06-12"
-UNTIL = "2020-06-22"
+# Slovenia
+# SINCE = "2020-06-12"
+# UNTIL = "2020-06-22"
+
+# Switzerland 2019
+SINCE = "2019-06-29"
+UNTIL = "2019-07-14"
+
+# NZ 2017/2018
+# SINCE = "2017-12-04"
+# UNTIL = "2018-02-15"
+
+# Canary Islands 2021
+# SINCE = "2021-02-26"
+# UNTIL = "2021-04-19"
 
 
 def decode_polyline(polyline_str):
@@ -48,22 +61,24 @@ def decode_polyline(polyline_str):
 
 def get_activities(access_token):
     headers = {"Authorization": f"Bearer {access_token}"}
-    after = datetime.fromisoformat(SINCE).timestamp()
-    before = datetime.fromisoformat(UNTIL).timestamp()
-    # data = {"before": 1617256800, "per_page": 10}
-    data = {
-        "before": before,
-        "after": after,
-        "per_page": 30
-    }
-    response = requests.get(
-        "https://www.strava.com/api/v3/athlete/activities",
-        headers=headers,
-        data=data
-    )
-    activities = response.json()
-    print(f"Got {len(activities)} activities")
-    return activities
+    data = {"per_page": 20}
+    if SINCE:
+        data["after"] = datetime.fromisoformat(SINCE).timestamp()
+    if UNTIL:
+        data["before"] = datetime.fromisoformat(UNTIL).timestamp()
+    data["page"] = 1
+    while True:
+        response = requests.get(
+            "https://www.strava.com/api/v3/athlete/activities",
+            headers=headers,
+            data=data
+        )
+        activities = response.json()
+        if activities:
+            data["page"] += 1
+            yield from activities
+        else:
+            break
 
 
 def main():
@@ -72,12 +87,24 @@ def main():
     activities = get_activities(access_token)
 
     the_map = folium.Map(tiles="Stamen Terrain")
-    for activity in activities:
+    for count, activity in enumerate(activities):
+        polyline_str = activity["map"]["summary_polyline"]
+        if not polyline_str:
+            continue
         points = decode_polyline(activity["map"]["summary_polyline"])
         folium.PolyLine(points, color="red").add_to(the_map)
+        popup_text = (
+            f"<b>{activity['name']}</b><br>\n"
+            f"Distance: {round(activity['distance']/1000, 1)} km<br>\n"
+            f"Elevation gain: {activity['total_elevation_gain']} m<br>\n"
+            f"<a href='https://www.strava.com/activities/{activity['id']}'>View on Strava</a>"
+        )
+        popup = folium.map.Popup(html=popup_text)
+        folium.Marker(location=points[-1], popup=popup).add_to(the_map)
     boundary = the_map.get_bounds()
     the_map.fit_bounds(boundary, padding=(3, 3))
     the_map.save("map.html")
+    print(f"Total activities: {count+1}")
     # print(f"poly: {poly}")
     # print(decode_polyline(poly))
 
