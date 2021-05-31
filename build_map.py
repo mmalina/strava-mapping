8e81036969c7923c0fa84165b063080f8d287f54
Martin Malina <mmalina@redhat.com>
2021-05-31 09:41:07 +0200

    Add photo thumbs + popups
    There is now a layercontrol in place with a checkbox
    to view the photos (off by default). The only drawback
    of including photos is that it takes 2 api calls per activity.
    The api limit is 100 calls per 15 minutes. So this means
    that my limit is around 48 activities per map, otherwise
    I will exceed the api limit.

--- build_map.py
+++ build_map.py
@@ -12,14 +12,16 @@ from get_access_token import get_access_token
 # Switzerland 2019
 SINCE = "2019-06-29"
 UNTIL = "2019-07-14"
+UNTIL = "2019-07-05"
 
 # NZ 2017/2018
 # SINCE = "2017-12-04"
 # UNTIL = "2018-02-15"
 
 # Canary Islands 2021
-SINCE = "2021-02-26"
-UNTIL = "2021-04-19"
+# SINCE = "2021-02-26"
+# SINCE = "2021-04-09"
+# UNTIL = "2021-04-19"
 
 
 def decode_polyline(polyline_str):
@@ -28,31 +30,31 @@ def decode_polyline(polyline_str):
     """
     index, lat, lng = 0, 0, 0
     coordinates = []
-    changes = {'latitude': 0, 'longitude': 0}
+    changes = {"latitude": 0, "longitude": 0}
 
     # Coordinates have variable length when encoded, so just keep
     # track of whether we've hit the end of the string. In each
     # while loop iteration, a single coordinate is decoded.
     while index < len(polyline_str):
         # Gather lat/lon changes, store them in a dictionary to apply them later
-        for unit in ['latitude', 'longitude']:
+        for unit in ["latitude", "longitude"]:
             shift, result = 0, 0
 
             while True:
                 byte = ord(polyline_str[index]) - 63
                 index += 1
-                result |= (byte & 0x1f) << shift
+                result |= (byte & 0x1F) << shift
                 shift += 5
                 if not byte >= 0x20:
                     break
 
-            if (result & 1):
+            if result & 1:
                 changes[unit] = ~(result >> 1)
             else:
-                changes[unit] = (result >> 1)
+                changes[unit] = result >> 1
 
-        lat += changes['latitude']
-        lng += changes['longitude']
+        lat += changes["latitude"]
+        lng += changes["longitude"]
 
         coordinates.append((lat / 100000.0, lng / 100000.0))
 
@@ -71,7 +73,7 @@ def get_activities(access_token):
         response = requests.get(
             "https://www.strava.com/api/v3/athlete/activities",
             headers=headers,
-            data=data
+            data=data,
         )
         activities = response.json()
         if activities:
@@ -81,13 +83,31 @@ def get_activities(access_token):
             break
 
 
+def get_activity_photos(access_token, activity_id, size=None):
+    headers = {"Authorization": f"Bearer {access_token}"}
+    data = {"photo_sources": True}
+    if size is not None:
+        data["size"] = size
+    response = requests.get(
+        f"https://www.strava.com/api/v3/activities/{activity_id}/photos",
+        headers=headers,
+        data=data,
+    )
+    response.raise_for_status()
+    return response.json()
+
+
 def main():
     access_token = get_access_token()
 
     activities = get_activities(access_token)
 
-    the_map = folium.Map()
+    the_map = folium.Map(tiles=None)
     folium.TileLayer("Stamen Terrain", detect_retina=True).add_to(the_map)
+    folium.TileLayer("OpenStreetMap", detect_retina=True).add_to(the_map)
+    fg = folium.FeatureGroup(name="Show Photos", show=False)
+    the_map.add_child(fg)
+    folium.LayerControl(collapsed=False).add_to(the_map)
     for count, activity in enumerate(activities):
         polyline_str = activity["map"]["summary_polyline"]
 
@@ -105,12 +125,27 @@ def main():
         popup = folium.map.Popup(html=popup_text)
         # folium.Marker(location=points[len(points)//2], popup=popup).add_to(the_map)
         folium.Marker(location=points[-1], popup=popup).add_to(the_map)
+        size = "400"
+        photos_thumb = get_activity_photos(access_token, activity["id"])
+        photos_large = get_activity_photos(access_token, activity["id"], size=size)
+        for photo in range(len(photos_thumb)):
+            if "location" not in photos_thumb[photo]:
+                continue
+            icon = folium.CustomIcon(
+                photos_thumb[photo]["urls"]["0"],
+                icon_size=photos_thumb[photo]["sizes"]["0"],
+            )
+            popup = folium.map.Popup(
+                html=f"<img src='{photos_large[photo]['urls'][size]}'>"
+            )
+            folium.Marker(
+                location=photos_thumb[photo]["location"], icon=icon, popup=popup
+            ).add_to(the_map).add_to(fg)
+
     boundary = the_map.get_bounds()
     the_map.fit_bounds(boundary, padding=(3, 3))
     the_map.save("map.html")
     print(f"Total activities: {count+1}")
-    # print(f"poly: {poly}")
-    # print(decode_polyline(poly))
 
 
 if __name__ == "__main__":
