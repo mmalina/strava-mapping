6d3caace118f437fa04863625e92f1f8f4db0778
Martin Malina <mmalina@redhat.com>
2024-06-27 12:53:46 +0200

    Setup dates for alps 2024

--- build_map.py
+++ build_map.py
@@ -28,9 +28,13 @@ ACTIVITIES_ENDPOINT = "https://www.strava.com/api/v3/athlete/activities"
 PHOTO_THUMB_SIZE = "64"
 PHOTO_LARGE_SIZE = "400"
 
+# Alps 2024
+SINCE = "2024-06-29"
+UNTIL = "2024-07-07"
+
 # Arizona Trail 2024
-SINCE = "2024-03-15"
-UNTIL = "2024-04-28"
+# SINCE = "2024-03-15"
+# UNTIL = "2024-04-28"
 
 # La Palma 2023
 # SINCE = "2023-11-29"
